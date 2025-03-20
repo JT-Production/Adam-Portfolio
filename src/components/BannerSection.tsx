fc6e705a93971767c7313d748365f023388ab578
JT-Production <joeyofficial707@gmail.com>
2025-03-20 19:12:28 +0100

    assd

--- src/components/BannerSection.tsx
+++ src/components/BannerSection.tsx
@@ -2,7 +2,6 @@ import Spline from "@splinetool/react-spline";
 import threeD from "../assets/vid/chips.mp4";
 import { BsCurrencyBitcoin } from "react-icons/bs";
 import { FaTelegram } from "react-icons/fa6";
-// import Button from "./Button";
 
 const BannerSection = () => {
   return (
@@ -15,20 +14,25 @@ const BannerSection = () => {
               ADAM
             </span>
             <div className="flex p-0 m-0">
-              {/* <BsCurrencyBitcoin  className='p-0 me-8'/> */}
               <span className="lg:text-9xl text-7xl font-bold flex ">
-               <BsCurrencyBitcoin  className='-translate-x-6 -translate-y-1 lg:relative md:relative absolute ' style={{color: 'orange'}}/> <div className='lg:-translate-x-12 cssanimation leBlurIn sequence lg:ms-0 md:ms-0 ms-9 '>ITCOIN</div>
+                <BsCurrencyBitcoin
+                  className="-translate-x-6 -translate-y-1 lg:relative md:relative absolute "
+                  style={{ color: "orange" }}
+                />{" "}
+                <div className="lg:-translate-x-12 cssanimation leBlurIn sequence lg:ms-0 md:ms-0 ms-9 ">
+                  ITCOIN
+                </div>
               </span>
             </div>
           </h1>
           <p className="text-start w-68 lg:text-md text-sm lg:w-auto mb-3">
             I teach marketing strategies , build thriving communities, create
-            content around all things crypto— helping brands and individual grow with
-            impact.
+            content around all things crypto— helping brands and individual grow
+            with impact.
           </p>
-          {/* <Button text='Learn More' link='' classNam='float-left' /> */}
-          <a 
-           href="https://t.me/AdamBitcoinP2P1"
+
+          <a
+            href="https://t.me/AdamBitcoinP2P1"
             className="cursor-pointer float-left bg-gradient-to-b mt-3 from--500 to--600  lg:px-8 lg:py-2 px-5 py-2 rounded-full border-[1px] border-orange-500 text-white font-medium group"
             style={{ border: "1px solid #FFA500" }}
           >
@@ -49,11 +53,10 @@ const BannerSection = () => {
               data-aos-duration="2000"
               className=" cursor-pointer hover:translate-x-20 transition delay-150 duration-300 ease-in-out blur-box text-white lg:translate-y-15 translate-x-33 p-3 border rounded-xl w-50 bg-white/5  shadow-lg shadow-gray-400/20 border-zinc-800 backdrop-filter backdrop-blur-xl bg-opacity-50 "
             >
-              {/* <h6 className='heading text-start font-bold mb-2'> Web3 Marketing Educator </h6> */}
               <p className="para  text-sm text-start">
                 I love to teach Marketing.
               </p>
-              {/* Guiding creators and <br /> startups to scale. */}
+
               <p></p>
             </div>
             <div
@@ -61,11 +64,10 @@ const BannerSection = () => {
               data-aos-duration="2000"
               className="a cursor-pointer hover:translate-x-6 transition delay-150 duration-300 ease-in-out blur-box text-white  translate-y-38 translate-x-15 p-3 border rounded-xl w-56 bg-white/5  shadow-lg shadow-gray-400/20 border-zinc-800 backdrop-filter backdrop-blur-xl bg-opacity-50 "
             >
-              {/* <h6 className='heading text-start font-bold mb-2'> Content creator</h6> */}
               <p className="para  text-sm text-start">
                 I create content revolving Web3
               </p>
-              {/* <br /> Guiding passionate builders  in creating thriving communities. */}
+
               <p></p>
             </div>
 
@@ -74,11 +76,10 @@ const BannerSection = () => {
               data-aos-duration="2000"
               className="a cursor-pointer hover:translate-x-20 transition delay-150 duration-300 ease-in-out blur-box text-white  translate-y-66 translate-x-25 p-3 border rounded-xl w-60 bg-white/5  shadow-lg shadow-gray-400/20 border-zinc-800 backdrop-filter backdrop-blur-xl bg-opacity-50 "
             >
-              {/* <h6 className='heading text-start font-bold mb-2'> Community builder</h6> */}
               <p className="para  text-sm text-start">
                 I love to teach community growth.
               </p>
-              {/* <br /> Guiding passionate builders  in creating thriving communities. */}
+
               <p></p>
             </div>
 
@@ -87,11 +88,10 @@ const BannerSection = () => {
               data-aos-duration="2000"
               className="a cursor-pointer  lg:block hidden  transition delay-150 duration-300 ease-in-out blur-box text-white  translate-y-72 translate-x-125 p-3 border rounded-xl w-41 lg:bg-white/5 md:bg-white/5 bg-white/30   shadow-lg shadow-gray-400/20 border-zinc-800 backdrop-filter backdrop-blur-xl bg-opacity-50 "
             >
-              {/* <h6 className='heading text-start font-bold mb-2'> Community builder</h6> */}
               <p className="para  text-sm font text-start">
                 Ex-Bitcoin Maximalist
               </p>
-              {/* <br /> Guiding passionate builders  in creating thriving communities. */}
+
               <p></p>
             </div>
           </div>
@@ -106,8 +106,6 @@ const BannerSection = () => {
             muted
             className=" object-cover lg:hidden block h-100"
           ></video>
-
-          {/* <Spline scene="https://prod.spline.design/IhDXjrf2RJyfcQ3b" className="3d"/> */}
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,14 @@
-import {  FaYoutube} from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
 import { Link } from "react-scroll";
 import { IoIosMenu } from "react-icons/io";
 import { LiaTimesSolid } from "react-icons/lia";
 import { useEffect, useState } from "react";
-import  Avatar  from "../assets/img/logo.jpg";
+import Avatar from "../assets/img/logo.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
 const Navbar = () => {
-  const [display, setDisplay ]= useState("hidden");
+  const [display, setDisplay] = useState("hidden");
 
   function handleClick() {
     setDisplay(display === "hidden" ? "block" : "hidden");
@@ -21,46 +20,64 @@ const Navbar = () => {
   return (
     <>
       <div className="nav flex justify-between items-center  align-center p-4 lg:px-8 pt-3 align-center">
-        <a href="" className="flex ms-3 gap-2 align-center cursor-pointer " data-aos="fade-right" data-aos-duration="2000">
-        <img src={Avatar} alt="" className="w-12 h-auto rounded-full " />
-          {/* <div className="logo lg:text-3xl text-xl text-white heading lg:mt-1.5 mt-2.5">Adam</div> */}
-  
-       
+        <a
+          href=""
+          className="flex ms-3 gap-2 align-center cursor-pointer "
+          data-aos="fade-right"
+          data-aos-duration="2000"
+        >
+          <img src={Avatar} alt="" className="w-12 h-auto rounded-full " />
         </a>
         <div
           data-aos="fade-down"
           data-aos-duration="1000"
           className="flex justify-between  items-center l right-0 font-bold lg:gap-8 gap-3 px-3 py-3 para lg:text-sm text-[13px] lg:heading backdrop-filter backdrop-blur-lg fixed bg-white/5 bg-opacity-30  lg:px-8 lg:py-4 rounded-4xl border- border-gray-200 me-3"
         >
-       
-          <Link to="about" smooth={true} duration={500}  className={`${display} lg:block cursor-pointer`}>
+          <Link
+            to="about"
+            smooth={true}
+            duration={500}
+            className={`${display} lg:block cursor-pointer`}
+          >
             About
           </Link>
-         
-          <Link to="channel" smooth={true} duration={500}  className={`${display} lg:block cursor-pointer flex gap-1 `}>
-          <div className="flex gap-1">
-          <FaYoutube className="lg:text-lg text-sm lg:pt-0 pt-0.5" /> Channel
-          </div>
-         
+
+          <Link
+            to="channel"
+            smooth={true}
+            duration={500}
+            className={`${display} lg:block cursor-pointer flex gap-1 `}
+          >
+            <div className="flex gap-1">
+              <FaYoutube className="lg:text-lg text-sm lg:pt-0 pt-0.5" />{" "}
+              Channel
+            </div>
           </Link>
-          <Link to="contact" smooth={true} duration={500}  href="" className={`${display} lg:block`}>
+          <Link
+            to="contact"
+            smooth={true}
+            duration={500}
+            href=""
+            className={`${display} lg:block`}
+          >
             Contact
           </Link>
-          {/* <a href="https://x.com/@AdamBitcoinP2P1" className={`${display} lg:block`}>
-            <FaXTwitter className="text-lg" />
-          </a>
-          <a href="https://t.me/AdamBitcoinChat" className={`${display} lg:block`}>
-            <FaTelegram className="text-lg" />
-          </a> */}
-            <a className={`lg:hidden ${display === "hidden" ? "block" : "hidden"}`} onClick={() => handleClick()}>
-            <IoIosMenu className="text-2xl" />
-            </a>
-            <a className={`lg:hidden ${display === "hidden" ? "hidden" : "block"} bg-white p-2 rounded-4xl`} onClick={() => handleClick()}>
-            <LiaTimesSolid className="text-2xl text-black" />
-            </a>
-        </div>
 
-       
+          <a
+            className={`lg:hidden ${display === "hidden" ? "block" : "hidden"}`}
+            onClick={() => handleClick()}
+          >
+            <IoIosMenu className="text-2xl" />
+          </a>
+          <a
+            className={`lg:hidden ${
+              display === "hidden" ? "hidden" : "block"
+            } bg-white p-2 rounded-4xl`}
+            onClick={() => handleClick()}
+          >
+            <LiaTimesSolid className="text-2xl text-black" />
+          </a>
+        </div>
       </div>
     </>
   );
